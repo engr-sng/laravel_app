0c6f3fd015e65f0eed2e7b23b1018699cc210436
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2022-11-03 17:21:25 +0900

    [add]シングルアクションコントローラー

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,36 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<E0F
-    <style>
-        body { font-size:16pt; color:#999; }
-        h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px  px;}
-    </style>
-    E0F;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt) {
-    return "<{$tag}>".$txt."</{$tag}>";
-}
-
 class HelloController extends Controller
 {
-    public function index() {
-        global $head, $style, $body, $end;
-
-        $html = $head.tag('title','Hello/Index').$style.
-            $body = tag('h1','Index').tag('p','this is Index page').'<a href="/hello/other">go to Other page</a>'.$end;
-        return $html;
-    }
-
-    public function other(){
-        global $head, $style, $body, $end;
-
-        $html = $head.tag('title','Hello/Other').$style.
-            $body = tag('h1','Other').tag('p','this is Other page').$end;
-        return $html;
+    public function __invoke() {
+        return <<<E0F
+        <html>
+            <head>
+            <title>Hello</title>
+                <style>
+                    body { font-size:16pt; color:#999; }
+                    h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px  px;}
+                </style>
+            </head>
+            <body>
+                <h1>Single Action</h1>
+                <p>これは、シングルアクションコントローラーのアクションです。</p>
+            </body>
+        </html>
+        E0F;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class,'index']);
-Route::get('/hello/other', [HelloController::class,'other']);
+Route::get('/hello', HelloController::class);
+
