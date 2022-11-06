0676377f4465cff62558ec2f57bd9ba9bf326750
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2022-11-06 15:00:22 +0900

    [add]ミドルウェアの設定

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index'])->middleware(HelloMiddleware::class);
 Route::post('/hello', [HelloController::class, 'post']);
